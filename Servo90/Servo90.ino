858b8e983656a4923af8202eb37497de3bed3bf0
mdequanter <dequanter@gmail.com>
2022-05-02 18:06:55 +0200

    Servo90

---
+++ Servo90/Servo90.ino
@@ -0,0 +1,44 @@
+/***********************************************************
+File name: servo90.ino
+Description: Power on, the servo rotates 90 to straighten the robotic arm
+Author: Maarten Dequanter
+Date: 02/05/2022
+***********************************************************/
+#include <Servo.h>。
+int servopin1 = 9;    //Define servo interface digital interface 9
+int servopin2 = 6;    //Define servo interface digital interface 6
+int servopin3 = 5;    //Define servo interface digital interface 5
+int servopin4 = 3;    //Define servo interface digital interface 3
+int servopin5 = 11;   //Define servo interface digital interface 11
+
+int moveServoData;
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+int angle = 90;        //Angle of rotation of the servo
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(servopin1,OUTPUT);//Set the servo interface as the output interface
+  pinMode(servopin2,OUTPUT);//Set the servo interface as the output interface
+  pinMode(servopin3,OUTPUT);//Set the servo interface as the output interface
+  pinMode(servopin4,OUTPUT);//Set the servo interface as the output interface
+  pinMode(servopin5,OUTPUT);//Set the servo interface as the output interface
+  servo1.attach(servopin1);
+  servo2.attach(servopin2);
+  servo3.attach(servopin3);
+  servo4.attach(servopin4);
+  servo5.attach(servopin5);
+  servo1.write(angle);
+  servo2.write(angle);
+  servo3.write(angle);
+  servo4.write(angle);
+  servo5.write(angle); 
+}
+
+void loop()
+{
+}
