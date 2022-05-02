05f2a8def96d384c8a3c5891e7112db0f2403e10
Maarten Dequanter <74420584+mdequanter@users.noreply.github.com>
2022-05-02 18:17:33 +0200

    Update Servo90.ino

--- Servo90/Servo90.ino
+++ Servo90/Servo90.ino
@@ -1,24 +1,33 @@
 /***********************************************************
 File name: servo90.ino
-Description: Power on, the servo rotates 90 to straighten the robotic arm
+Description: Opstarten van Robot arm, alle servo's in initiële stand zeten
 Author: Maarten Dequanter
 Date: 02/05/2022
 ***********************************************************/
-#include <Servo.h>。
-int servopin1 = 9;    //Define servo interface digital interface 9
-int servopin2 = 6;    //Define servo interface digital interface 6
-int servopin3 = 5;    //Define servo interface digital interface 5
-int servopin4 = 3;    //Define servo interface digital interface 3
-int servopin5 = 11;   //Define servo interface digital interface 11
 
-int moveServoData;
+// We laden de standaard arduino bibliotheek
+#include <Servo.h>
 
+// Initialisatie van servo aansluitingen
+int servopin1 = 9;    //Servo 1 is verbonden met digitale poort 9
+int servopin2 = 6;    
+int servopin3 = 5;    
+int servopin4 = 3;    
+int servopin5 = 11;   
+
+// Initialisatie van Servo objecten 1 object per servo.
 Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
 Servo servo5;
-int angle = 90;        //Angle of rotation of the servo
+int angle = 90;        //Variabele om servo's in te stellen
+
+
+/*
+Eerste stellen we alles in.  We zorgen voor het configureren van de in of uitgangspinnen, stellen de servo's in 
+en schrijven ineens de juiste waarde(angle) naar de servo objecten waardoor de servo's in de juiste stand gaan staan.
+*/
 
 void setup() {
   // put your setup code here, to run once:
@@ -38,6 +47,10 @@ void setup() {
   servo4.write(angle);
   servo5.write(angle); 
 }
+
+/*
+Deze loop is leeg omdat we slechts 1 maal een initialisatie van de servo's moeten doen.
+*/
 
 void loop()
 {
